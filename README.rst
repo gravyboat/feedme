de3bcfa998f8b80a281a867ee457e58843a72057
Forrest Alvarez <forrest.alvarez@gmail.com>
2014-05-24 18:33:56 -0400

    Updated the readme with some details, and populated some of the base files,

---
+++ README.rst
@@ -0,0 +1,6 @@
+Feedme
+======
+
+Feedme is an RSS tracking podcast application which allows you to track your
+favorite podcasts while ensuring you don't lose your place while switching
+between your home and the office.
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='Feedme',
+      version='1.0',
+      description='Rss Podcast Tracker',
+      author='Forrest Alvarez',
+      py_modules=['feedme'],
+      install_require = ['flask', 'feedstail'],
+     )
